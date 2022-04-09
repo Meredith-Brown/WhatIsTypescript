ab25be65984be310b7c70ec2415a15675ca680d9
meredith <meredithbrown302@gmail.com>
2022-04-09 16:50:30 -0400

    some progress

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,60 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReactDOM from 'react-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
+const axios = require('axios');
 
 function App() {
 
-  const form: HTMLFormElement = document.querySelector('#defineform');
-  document.body.addEventListener("submit", async function(event){
-  event.preventDefault();
-  // let form = event.target as HTMLFormElement;
-  // it seems like this means form as in a form with info. to be input
-  // https://www.javascripttutorial.net/javascript-dom/javascript-form/
-      });
+  const [def, setDef] = useState([]);
 
-form.onsubmit = () => {
-  const formData = new FormData(form);
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
-      .then((response: Response) => response.json())
-      .then(function(data){console.log(data)})
-      .catch((error) => console.log(error));
-  console.log(text);
-  return true; // prevent reload
+  const form: HTMLFormElement = document.querySelector("#defineform");
+  document.body.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    let form = event.target as HTMLFormElement;
+  });
 
-};
-
-ReactDOM.render(
-  <div>
-  <h1>Hello, Welcome to React and TypeScript</h1>
-  </div>,
-    document.getElementById("root")
-  );
+    useEffect(() => {
+      const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+      form.onsubmit = async () => {
+        const formData = new FormData(form);
+        // console.log(formData);
+        const text = formData.get("defineword") as string;
+        axios.get(url + text)
+          .then((res) => {
+            console.log(res);
+            setDef(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+        })
+      }
+    }
+      ,[]);
   
   return (
     <div className="App">
-      <main className="container">
-  <div className="bg-light p-5 rounded">
-    <h1>Definition</h1>
-    <p className="lead">Use this part of the page to present your results from the API call.</p>
-    <ul className="list-unstyled">
-        <li>This is a list. Your word could go here.</li>
-        <li>It appears completely unstyled.</li>
-        <li>Maybe your multiple definitions of the word are here?</li>
-        <li>Structurally, it's still a list.</li>
-        <li>However, this style only applies to immediate child elements.</li>
-        <li>Nested lists: (maybe synonyms and antonyms?)
-          <ul>
-            <li>are unaffected by this style</li>
-            <li>will still show a bullet</li>
-            <li>and have appropriate left margin</li>
-          </ul>
-        </li>
-        <li>This may still come in handy in some situations.</li>
+<main className="container">
+
+<div className="bg-light p-5 rounded">
+  <h1 className="defined">Definition</h1>
+  <p className="lead">Random Text</p>
+  {def.map((def) => (
+  <ul className="list-unstyled">
+    <li>{def.word}</li>
+    <li>{def.phonetic}</li>
+    <li>{def.meanings[0].antonyms}</li>
+    <li>{def.meanings[0].definitions[0].defintion}</li>
+
+      <li>Random Text</li>
+    <li>Maybe your multiple definitions of the word are here?</li>
+    <li>Structurally, it's still a list.</li>
+    <li>
+      However, this style only applies to immediate child elements.
+    </li>
+    <li>
+      Nested lists: (maybe synonyms and antonyms?)
+      <ul>
+        <li>are unaffected by this style</li>
+        <li>will still show a bullet</li>
+        <li>and have appropriate left margin</li>
       </ul>
-  </div>
+    </li>
+    <li>This may still come in handy in some situations.</li>
+    </ul>
+    ))}
+ </div>
+
 </main>
     </div>
   );
